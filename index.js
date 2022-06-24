5d160f8524ac4e239c9540231cd47b1ef9961c90
Your Name <you@example.com>
2022-06-24 15:47:23 -0300

    layout da lista

---
+++ index.js
@@ -0,0 +1,9 @@
+const form = document.querySelector(".users");
+const input = document.querySelector(".form input");
+const button = document.querySelector(".btn");
+
+button.onclick = e => {
+    e.preventDefault();
+    alert("Enviado com sucesso!");
+   
+}
